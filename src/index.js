9a9fea0a9c3bda21fc69a00cfb128980c54df42d
Vitaliy Saburdo <vsaburdo@gmail.com>
2022-11-22 23:32:28 +0200

    add js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
 
 
+
+function ringsApi() {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '31530619-c8ff0ea55f4ca3c44478c1e7e';
+  return fetch(`${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`).then(resp => {
+    if (!resp.ok) {
+    throw new Error(resp.statusText)
+    }
+  return resp.json()}).then(data => console.log(data))
+}
+
+// Your API key: 31530619-c8ff0ea55f4ca3c44478c1e7e
+
 function renderList(data) {
   const imageItem = data
     .map(({
