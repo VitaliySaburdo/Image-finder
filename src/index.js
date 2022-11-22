aabe547f31a1a87f18c1f3dc080ff12c7bbf8fbe
VitaliySaburdo <vsaburdo@gmail.com>
2022-11-22 20:06:39 +0200

    add

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import './css/styles.css';
+
+function renderList(data) {
+  const imageItem = data
+    .map(() => {
+      `<div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join();
+  cardsContainer.innerHTML = imageItem;
+}
