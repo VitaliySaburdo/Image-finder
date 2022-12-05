02f52627075f0ec0b1c02fa845a07a91c62040bd
VitaliySaburdo <vsaburdo@gmail.com>
2022-12-05 10:17:28 +0200

    fix

--- src/js/fetchName.js
+++ src/js/fetchName.js
@@ -10,7 +10,16 @@ export default class NewsApiService {
   async fetchName() {
     const BASE_URL = 'https://pixabay.com/api/';
     const API_KEY = '31530619-c8ff0ea55f4ca3c44478c1e7e';
-    const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.per_page}`;
+    const searchParams = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: this.page,
+      per_page: this.per_page,
+    });
+    const url = `${BASE_URL}?${searchParams}`;
 
     return await axios.get(url).then(async response => {
       try {
